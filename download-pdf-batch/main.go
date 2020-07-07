cb8ccfadc524d7565cc0c57900629898086451bd
Kasper Olsen <kols@danskerhverv.dk>
2020-07-07 15:41:59 +0200

    restruct project for modules and started download batch project

---
+++ download-pdf-batch/main.go
@@ -0,0 +1,68 @@
+package main
+
+import (
+	"io"
+	"log"
+	"net/http"
+	"os"
+	"sync"
+	"time"
+
+	"github.com/svopper/go-tools/utils/bufio"
+)
+
+// DownloadFile will download a url to a local file. It's efficient because it will
+// write as it downloads and not load the whole file into memory.
+func DownloadFile(filepath string, url string) error {
+
+	// Get the data
+	resp, err := http.Get(url)
+	if err != nil {
+		return err
+	}
+	defer resp.Body.Close()
+
+	// Create the file
+	out, err := os.Create(filepath)
+	if err != nil {
+		return err
+	}
+	defer out.Close()
+
+	// Write the body to file
+	_, err = io.Copy(out, resp.Body)
+	return err
+}
+
+func main() {
+	inputFilePath := os.Args[1]
+
+	lines, err := bufio.ReadLines(inputFilePath)
+	if err != nil {
+		panic(err)
+	}
+
+	outputPath := "out"
+	if _, err := os.Stat(outputPath); os.IsNotExist(err) {
+		os.Mkdir(outputPath, os.ModeDir)
+	}
+
+	linesLength := len(lines)
+
+	var wg sync.WaitGroup
+	wg.Add(linesLength)
+	start := time.Now()
+	for i := 0; i < linesLength; i++ {
+		go func(i int) {
+			defer wg.Done()
+			err := DownloadFile("logo.svg", lines[i])
+			if err != nil {
+				panic(err)
+			}
+		}(i)
+	}
+	wg.Wait()
+	elapsed := time.Since(start)
+	log.Printf("Gen took %s", elapsed)
+
+}
--- go.mod
+++ go.mod
@@ -1,5 +1,5 @@
-module github.com/svopper/qr-code-gen
+module github.com/svopper/go-tools
 
 go 1.14
 
-require github.com/yeqown/go-qrcode v1.2.1 // direct
+require github.com/yeqown/go-qrcode v1.2.1
