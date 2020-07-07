e7f603c7ae36aa2c8004fe6142a946ba230251e5
Kasper Olsen <kols@danskerhverv.dk>
2020-07-07 16:07:01 +0200

    added readme

---
+++ README.md
@@ -0,0 +1,23 @@
+# Go Tools
+
+This repo is a collection of handy tools, written in Go, I create when I stumble upon a tedious manual task.
+
+## Usage
+
+`cd` into the tool you wish to use and run
+
+```bash
+go run main.go <parameters>
+```
+
+## Contributing
+
+Pull requests are welcome. For major changes, please open an issue first to discuss what you would like to change.
+
+## License
+
+```go
+func GetLicense() interface{} {
+   return nil
+}
+```
--- qr-code-gen/main.go
+++ qr-code-gen/main.go
@@ -1,38 +1,21 @@
 package main
 
 import (
-	"bufio"
 	"fmt"
 	"log"
 	"os"
 	"sync"
 	"time"
 
+	"github.com/svopper/go-tools/utils/bufio"
 	"github.com/yeqown/go-qrcode"
 )
-
-// readLines reads a whole file into memory
-// and returns a slice of its lines.
-func readLines(path string) ([]string, error) {
-	file, err := os.Open(path)
-	if err != nil {
-		return nil, err
-	}
-	defer file.Close()
-
-	var lines []string
-	scanner := bufio.NewScanner(file)
-	for scanner.Scan() {
-		lines = append(lines, scanner.Text())
-	}
-	return lines, scanner.Err()
-}
 
 func main() {
 
 	inputFilePath := os.Args[1]
 
-	lines, err := readLines(inputFilePath)
+	lines, err := bufio.ReadLines(inputFilePath)
 	if err != nil {
 		panic(err)
 	}
